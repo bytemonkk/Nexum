9f6f9c4239e42ef885c2eaf52e17245e1b49db21
byteMonk <bytemonk71@gmail.com>
2026-09-13 17:50:22 +0530

    implemented basic http over tcp

--- tcp_server.py
+++ tcp_server.py
@@ -44,7 +44,7 @@ print(f"Received:\n{data.decode()}")
 response = (
     "HTTP/1.1 200 OK\r\n"
     "Content-Type: text/plain\r\n"
-    "Content-Length: 16\r\n"
+    "Content-Length: 17\r\n"
     "\r\n"
     "Hello from Nexus"
 )
