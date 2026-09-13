58ed3b64f08f0083706c41ed8606660c7b57340e
byteMonk <bytemonk71@gmail.com>
2026-09-13 18:30:56 +0530

    handle malformed http requests

--- tcp_server.py
+++ tcp_server.py
@@ -1,27 +1,7 @@
-# import socket
-
-# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# server.bind(("127.0.0.1", 8000))
-
-# server.listen()
-
-# print("Nexum is listening on 127.0.0.1:8000")
-
-# connection, address = server.accept()
-
-# print(f"Client connected: {address}")
-
-# data = connection.recv(1024)
-
-# print(f"Received: {data!r}")
-
-# connection.sendall(b"Hello from Nexum")
-
-# connection.close()
-# server.close()
-
 import socket
+
+from http_parser import HttpParseError, parse_request
+
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -37,16 +17,37 @@ print(f"Client connected: {address}")
 
 data = connection.recv(4096)
 
-print(f"Received:\n{data.decode()}")
+try:
+    request = parse_request(data)
 
-# connection.sendall(b"Hello from Nexum")
+except HttpParseError as error:
+    print(f"HTTP parsing failed: {error}")
+
+    response = (
+        "HTTP/1.1 400 Bad Request\r\n"
+        "Content-Type: text/plain\r\n"
+        "Content-Length: 11\r\n"
+        "\r\n"
+        "Bad Request"
+    )
+
+    connection.sendall(response.encode())
+
+    connection.close()
+    server.close()
+    exit()
+
+print("METHOD:", request.method)
+print("PATH:", request.path)
+print("VERSION:", request.version)
+print("HEADERS:", request.headers)
 
 response = (
     "HTTP/1.1 200 OK\r\n"
     "Content-Type: text/plain\r\n"
-    "Content-Length: 17\r\n"
+    "Content-Length: 5\r\n"
     "\r\n"
-    "Hello from Nexus"
+    "Hello"
 )
 
 connection.sendall(response.encode())
