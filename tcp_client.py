2621201878539fc14a7ef97bb27bd9029e095202
byteMonk <bytemonk71@gmail.com>
2026-09-13 17:49:43 +0530

    implemented raw tcp client server

--- tcp_client.py
+++ tcp_client.py
@@ -1,13 +1,33 @@
+# import socket
+
+# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# client.connect(("127.0.0.1", 8000))
+
+# client.sendall(b"Hello Nexum")
+
+# data = client.recv(1024)
+
+# print(f"Server replied: {data!r}")
+
+# client.close()
+
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect(("127.0.0.1", 8000))
 
-client.sendall(b"Hello Nexum")
+request = (
+    "GET / HTTP/1.1\r\n"
+    "Host: localhost\r\n"
+    "\r\n"
+)
 
-data = client.recv(1024)
+client.sendall(request.encode())
 
-print(f"Server replied: {data!r}")
+data = client.recv(4096)
+
+print(data.decode())
 
 client.close()
--- tcp_server.py
+++ tcp_server.py
@@ -1,3 +1,26 @@
+# import socket
+
+# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# server.bind(("127.0.0.1", 8000))
+
+# server.listen()
+
+# print("Nexum is listening on 127.0.0.1:8000")
+
+# connection, address = server.accept()
+
+# print(f"Client connected: {address}")
+
+# data = connection.recv(1024)
+
+# print(f"Received: {data!r}")
+
+# connection.sendall(b"Hello from Nexum")
+
+# connection.close()
+# server.close()
+
 import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,11 +35,21 @@ connection, address = server.accept()
 
 print(f"Client connected: {address}")
 
-data = connection.recv(1024)
+data = connection.recv(4096)
 
-print(f"Received: {data!r}")
+print(f"Received:\n{data.decode()}")
 
-connection.sendall(b"Hello from Nexum")
+# connection.sendall(b"Hello from Nexum")
+
+response = (
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Type: text/plain\r\n"
+    "Content-Length: 16\r\n"
+    "\r\n"
+    "Hello from Nexus"
+)
+
+connection.sendall(response.encode())
 
 connection.close()
 server.close()
